0e4a87be27f0be0fe58857b45e9e5e50c915dc5b
EpocDotFr <contact@epoc.fr>
2022-09-13 16:13:13 +0200

    Try to fix problem 0

--- 0_smoke_test.py
+++ 0_smoke_test.py
@@ -4,12 +4,12 @@ from protohacker_server import ProtohackerHandler, run_server
 class SmokeTestHandler(ProtohackerHandler):
     def handle(self):
         while True:
-            data = self.rfile.readline()
+            data = self.rfile.read(1)
+
+            self.log_data(data)
 
             if not data:
                 break
-
-            self.log_data(data)
 
             self.wfile.write(data)
 
